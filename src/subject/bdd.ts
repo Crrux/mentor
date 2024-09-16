d6e8d6995aaef72b8aa453fe8a082e0a3d17aa60
Mursch Arthur <arthur.mursch@gmail.com>
2024-09-16 16:39:20 +0200

    save

---
+++ src/subject/bdd.ts
@@ -0,0 +1,6 @@
+export const SUBJECTS = [
+  { id: 1, name: 'francais' },
+  { id: 2, name: 'anglais' },
+  { id: 3, name: 'math' },
+  { id: 4, name: 'physique' },
+];
---
+++ src/subject/subject.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { SUBJECTS } from './bdd';
+import { InterfacePostSubject, InterfaceSubject } from './subject';
+import { SubjectService } from './subject.service';
+
+@Controller('subject')
+export class SubjectController {
+  constructor(private readonly subjectService: SubjectService) {}
+
+  @Get()
+  findAll(): InterfaceSubject[] {
+    return SUBJECTS;
+  }
+
+  @Get(':id')
+  findOneById(@Param('id') id: string): InterfaceSubject {
+    return this.subjectService.FindOneById(+id);
+  }
+
+  @Post()
+  addSubject(@Body() subject: InterfacePostSubject): InterfaceSubject[] {
+    return this.subjectService.CreateNewSubject(subject);
+  }
+}
