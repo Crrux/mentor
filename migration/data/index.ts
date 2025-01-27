b50694cf528c5c0d55a80b52c9967b21ebde8e6c
Mursch Arthur <arthur.mursch@gmail.com>
2025-01-27 14:30:07 +0100

    save

---
+++ migration/data/index.ts
@@ -0,0 +1,22 @@
+var initSubject = [
+  { name: 'Mathématiques', levelId: 3 },
+  { name: 'Francais', levelId: 2 },
+  { name: 'SVT', levelId: 3 },
+  { name: 'Histoire-Géographie', levelId: 2 },
+  { name: 'Education Civique', levelId: 4 },
+  { name: 'Langue vivante etrangère', levelId: 3 },
+  { name: 'Arts plastiques', levelId: 1 },
+  { name: 'EPS', levelId: 1 },
+  { name: 'Technologie', levelId: 5 },
+  { name: 'Musique', levelId: 5 },
+  { name: 'Informatique', levelId: 4 },
+  { name: 'Phylosophie', levelId: 4 },
+];
+
+var initLevel = [
+  { name: 'Elementaire' },
+  { name: 'Maternelle' },
+  { name: 'College' },
+  { name: 'Lycee' },
+  { name: 'Universite' },
+];
