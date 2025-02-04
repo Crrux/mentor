11406c02725dc8c191f56c44d43d7d2e8048ad1d
Mursch Arthur <arthur.mursch@gmail.com>
2025-02-04 13:55:35 +0100

    ajout configModule

--- src/subject/subject.service.ts
+++ src/subject/subject.service.ts
@@ -1,5 +1,6 @@
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Cache } from 'cache-manager';
 import { Repository } from 'typeorm';
@@ -12,6 +13,7 @@ export class SubjectService {
     @InjectRepository(SubjectEntity)
     private subjectRepository: Repository<SubjectEntity>,
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private configServive: ConfigService,
   ) {}
 
   async findAll(): Promise<SubjectEntity[]> {
@@ -48,6 +50,6 @@ export class SubjectService {
   }
 
   findFavorite(): string {
-    return 'Maths';
+    return this.configServive.get<string>('FAVORITE_SUBJECT');
   }
 }
